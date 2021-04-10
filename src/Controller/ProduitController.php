c084364b7485cbb6bf6677423a6bcc60499388c3
cguill18 <clementineguillot@gmail.com>
2021-04-10 18:57:38 +0200

    formulaires sur les produits du magasin

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -17,7 +17,7 @@ class ProduitController extends AbstractController
      * @return Response
      * @Route ("/ajoutProduits", name= "produit_ajout_produit")
      */
-    public function ajoutProduitsAction(Request $request):Response
+    public function ajoutProduitsAction(Request $request): Response
     {
         $user = $this->getParameter('user');
         $em = $this->getDoctrine()->getManager();
@@ -27,19 +27,59 @@ class ProduitController extends AbstractController
             throw new NotFoundHttpException("Vous n'avez pas les droits pour accéder à cette page.");
         else {
             $produit = new Produits();
-            $form = $this->createForm(ProduitType::class,$produit);
-            $form->add('send', SubmitType::class,['label'=>'ajouter produit']);
+            $form = $this->createForm(ProduitType::class, $produit);
+            $form->add('send', SubmitType::class, ['label' => 'ajouter produit']);
             $form->handleRequest($request);
-            if ($form->isSubmitted() && $form->isValid())
-            {
+            if ($form->isSubmitted() && $form->isValid()) {
                 $em->persist($produit);
                 $em->flush();
-                $this->addFlash('info','Le produit a été ajouter avec succes.');
+                $this->addFlash('info', 'Le produit a été ajouter avec succes.');
                 return $this->redirectToRoute('accueil');
             }
 
-            $args = array('formAjoutProduit'=>$form->createView());
-            return $this->render("Produit/ajoutProduit.html.twig",$args);
+            $args = array('formAjoutProduit' => $form->createView());
+            return $this->render("Produit/ajoutProduit.html.twig", $args);
         }
     }
+
+    /**
+     * @param Request $request
+     * @return Response
+     * @Route ("/voirMagasin", name="produit_magasin")
+     */
+    public function voirMagasinAction(Request $request): Response
+    {
+        $user = $this->getParameter('user');
+        $em = $this->getDoctrine()->getManager();
+        $utilisateurRepository = $em->getRepository('App:Utilisateur');
+        $utilisateur = $utilisateurRepository->find($user);
+        if (is_null($utilisateur) || ($utilisateur->getIsadmin()))
+            throw new NotFoundHttpException("Vous n'avez pas les droits pour accéder à cette page.");
+        else {
+            $form = $request->request;
+
+            if(is_null($form)){
+            } else {
+                foreach ($form as $k => $v){
+                    dump($k);
+                    dump($v);
+                    //faire ope sur le panier
+                }
+            }
+
+            $produitRepository = $em->getRepository('App:Produits');
+            $produits = $produitRepository->findAll();
+            $args = array('produits' => $produits);
+            return $this->render("Produit/magasin.html.twig", $args);
+        }
+    }
+
+    /**
+     * @Route ("/traitMagasin", name="produit_trait_magasin")
+     */
+    public function traitMagasinAction(Request $request): Response
+    {
+
+    }
+
 }
