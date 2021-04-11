956595acbebdc6f8480fd4c7cd8377a44abcee11
Louis Forestier <louis.forestier@etu.univ-poitiers.fr>
2021-04-11 17:44:08 +0200

    ajout assert produit

--- src/Entity/Produit.php
+++ src/Entity/Produit.php
@@ -6,6 +6,7 @@ use App\Repository\ProduitsRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Table (name="im2021_produits")
@@ -22,16 +23,21 @@ class Produit
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Le libellé est obligatoire.")
      */
     private $libelle;
 
     /**
      * @ORM\Column(type="float")
+     * @Assert\NotBlank(message="Le prix est obligatoire.")
+     * @Assert\PositiveOrZero(message="Le prix doit être supérieur ou égal à 0.")
      */
     private $prix;
 
     /**
      * @ORM\Column(type="integer")
+     * @Assert\NotBlank(message="La quantité est obligatoire.")
+     * @Assert\PositiveOrZero(message="La quantité doit être supérieure ou égale à 0.")
      */
     private $quantite;
 
